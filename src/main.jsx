b54dbccb20b86a46ec9865e5dabe1f85b2d25dc6
Nusrat Jahan Raisa <jahanthinks@gmail.com>
2025-07-21 22:47:53 +0600

    router,library setup

---
+++ src/main.jsx
@@ -0,0 +1,37 @@
+import { Children, StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+import MainLayout from './Components/MainLayout/MainLayout';
+import Home from './Components/Pages/Home';
+import SignUp from './Components/Pages/SignUp';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout></MainLayout>,
+    Children: [
+      {
+        path: "/",
+        element: <Home></Home>
+      },
+      {
+        path: "/signup",
+        element: <SignUp></SignUp>
+      }
+    ]
+  },
+]);
+
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+     <RouterProvider router={router} />
+  </StrictMode>,
+)
